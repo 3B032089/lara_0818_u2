1d35e0ed1f401c1e87786158e2d2172459875af1
Fuuzuki0307 <Yaesakura716207@gmail.com>
2023-11-22 21:08:48 +0800

    傳遞資料至Views顯示

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,6 @@ Route::get('/', function () {
 });
 
 Route::get('/home',[HomeController::class,'index'])->name('home.index');
+
+Route::get('/hello',[HelloController::class,'index'])->name('hello.index');
+
