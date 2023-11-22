a0ebb724971ce9ebdf8ca7db4f834fdc7278ad57
Fuuzuki0307 <Yaesakura716207@gmail.com>
2023-11-22 20:36:17 +0800

    改用HomeControler回傳

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home',function(){
-    return view('home.index');
-});
+Route::get('/home',[HomeController::class,'index'])->name('home.index');
